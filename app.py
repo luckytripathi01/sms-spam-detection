8b75173633a44e0bca8a409d82a8f907f9087804
Lucky Tripathi <luckytripathi4012@gmail.com>
2026-02-25 23:40:24 +0530

    NLTK stopwords deployment error

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import nltk
 import string
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-
+nltk.download('punkt')
+nltk.download('stopwords')
 ps = PorterStemmer()
 
 # ---------------- TEXT TRANSFORM ----------------
